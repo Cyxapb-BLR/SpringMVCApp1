c85ab4c51ab8ec1acdecb29df149ad516d80ebb3
Cyxapb-BLR <cyxapb-511@yandex.ru>
2021-05-12 12:55:24 -0400

    Параметры GET запроса с помощью HttpServletRequest

--- src/main/java/com/matskevich/springcource/config/controllers/FirstController.java
+++ src/main/java/com/matskevich/springcource/config/controllers/FirstController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/first") //если убрать то будет работать http://localhost:8080/goodbye http://localhost:8080/hello,
 // а с этим нужно в адрес добавлять /first
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String lastname = request.getParameter("lastname");
+        System.out.println("Hello " + name + " " + lastname);
         return "first/hello";
     }
 
