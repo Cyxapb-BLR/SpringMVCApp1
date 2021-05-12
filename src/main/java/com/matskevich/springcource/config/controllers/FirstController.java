31b7a33be4fc2246db1b896903811923ce7917b1
Cyxapb-BLR <cyxapb-511@yandex.ru>
2021-05-12 13:25:50 -0400

    Заменил HttpServletRequest на @RequestParam, добавил ссылку с параметрами в hello.html

--- src/main/java/com/matskevich/springcource/config/controllers/FirstController.java
+++ src/main/java/com/matskevich/springcource/config/controllers/FirstController.java
@@ -3,6 +3,7 @@ package com.matskevich.springcource.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,10 +11,14 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first") //если убрать то будет работать http://localhost:8080/goodbye http://localhost:8080/hello,
 // а с этим нужно в адрес добавлять /first
 public class FirstController {
+    /*@GetMapping("/hello")
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam("lastname") String lastname) {*/
+    //если написать так, то при вводе url без параметров будет ошибка
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String lastname = request.getParameter("lastname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "lastname", required = false) String lastname) {
+        // когда написали value и required=false,то url можно не передавать параметры, ошибки не будет, параметры будут null
         System.out.println("Hello " + name + " " + lastname);
         return "first/hello";
     }
